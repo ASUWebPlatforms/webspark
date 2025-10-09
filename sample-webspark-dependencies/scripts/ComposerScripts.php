2edbda53b0a1fe180ded016386c857fb6f38582b
Travis Butterfield <tbutterf@asu.edu>
2025-10-09 16:29:38 -0700

    Initial commit

---
+++ sample-webspark-dependencies/scripts/ComposerScripts.php
@@ -0,0 +1,208 @@
+<?php
+
+/**
+ * @file
+ * Contains \ComposerScripts.
+ *
+ * Custom Composer scripts and implementations of Composer hooks.
+ */
+
+namespace SampleWebsparkCustomScripts;
+
+use Composer\Plugin\PreCommandRunEvent;
+use Composer\Script\Event;
+
+/**
+ * Implementation for Composer scripts and Composer hooks.
+ */
+class ComposerScripts {
+
+  /**
+   * Prepare for Composer to update dependencies.
+   *
+   * Composer will attempt to guess the version to use when evaluating
+   * dependencies for path repositories. This has the undesirable effect
+   * of producing different results in the composer.lock file depending on
+   * which branch was active when the update was executed.
+   *
+   * To work around this problem, it is possible to define an environment
+   * variable that contains the version to use whenever Composer would normally
+   * "guess" the version from the git repository branch. We set this invariantly
+   * to "dev-main" so that the composer.lock file will not change if the same
+   * update is later ran on a different branch.
+   *
+   * @see https://github.com/composer/composer/blob/main/doc/articles/troubleshooting.md#dependencies-on-the-root-package
+   */
+  public static function preUpdate(Event $event) {
+    $io = $event->getIO();
+
+    // We will only set the root version if it has not already been overriden
+    if (!getenv('COMPOSER_ROOT_VERSION')) {
+      // This is not an error; rather, we are writing to stderr.
+      $io->writeError("<info>Using version 'dev-main' for path repositories.</info>");
+
+      putenv('COMPOSER_ROOT_VERSION=dev-main');
+    }
+
+    // Apply updates to top-level composer.json
+    static::applyComposerJsonUpdates($event);
+  }
+
+  /**
+   * postUpdate
+   *
+   * After "composer update" runs, we have the opportunity to do additional
+   * fixups to the project files.
+   *
+   * @param \DrupalComposerManaged\Composer $event
+   *   The Event object passed in from Composer
+   */
+  public static function postUpdate(Event $event) {
+    $io = $event->getIO();
+
+    // Check for FontAwesome library's existence
+    $fa_install = is_dir('docroot/libraries/fontawesome');
+    if ($fa_install) {
+      // Get ASUAwesome icons icon list.
+      $asuawesome_icon_list = file_get_contents('docroot/profiles/contrib/webspark/themes/renovation/asuawesome-iconlist.yml');
+      // Custom Renovation ASUAwesome icon list.
+      $renovation_icon_list = 'docroot/profiles/contrib/webspark/themes/renovation/renovation.fontawesome.iconlist.yml';
+      // Write ASUAwesome icons to the ASUAwesome custom icon list.
+      file_put_contents($renovation_icon_list, $asuawesome_icon_list);
+      // Get Font Awesome icon list.
+      $font_awesome_icons = file_get_contents('docroot/libraries/fontawesome/metadata/icons.yml');
+      // Append FontAwesome icons to the ASUAwesome custom icon list.
+      file_put_contents($renovation_icon_list, $font_awesome_icons, FILE_APPEND);
+
+      // Write out success message to console.
+      $io->writeError("<info>Successfully combined ASUAwesome and Font Awesome icons</info>");
+    }
+  }
+
+  /**
+   * Apply composer.json Updates
+   *
+   * During the Composer pre-update hook, check to see if there are any
+   * updates that need to be made to the composer.json file. We cannot simply
+   * change the composer.json file in the upstream, because doing so would
+   * result in many merge conflicts.
+   */
+  public static function applyComposerJsonUpdates(Event $event) {
+    $io = $event->getIO();
+
+    $composerJsonContents = file_get_contents("composer.json");
+    $composerJson = json_decode($composerJsonContents, true);
+    $originalComposerJson = $composerJson;
+
+    // add our post-update-cmd hook if it's not already present
+    $our_hook = 'WebsparkCustomScripts\\ComposerScripts::postUpdate';
+    // if does not exist, add as an empty arry
+    if(! isset($composerJson['scripts']['post-update-cmd'])) {
+      $composerJson['scripts']['post-update-cmd'] = [];
+    }
+
+    // if exists and is a string, convert to a single-item array (n.b. do not actually need the if exists check because we just assured that it does)
+    if(is_string($composerJson['scripts']['post-update-cmd'])) {
+      $composerJson['scripts']['post-update-cmd'] = [$composerJson['scripts']['post-update-cmd']];
+    }
+
+    // if exists and is an array and does not contain our hook, add our hook (again, only the last check is needed)
+    if(! in_array($our_hook, $composerJson['scripts']['post-update-cmd'])) {
+      $io->write("<info>Adding post-update-cmd hook to composer.json</info>");
+      $composerJson['scripts']['post-update-cmd'][] = $our_hook;
+
+      // We're making our other changes if and only if we're already adding our hook
+      // so that we don't overwrite customer's changes if they undo these changes.
+      // We don't want customers to remove our hook, so it will be re-added if they remove it.
+
+      // Remove our upstream convenience scripts, if the user has not removed them.
+      if (isset($composerJson['scripts']['upstream-require'])) {
+        unset($composerJson['scripts']['upstream-require']);
+      }
+
+      // Also remove it from the scripts-descriptions section.
+      if (isset($composerJson['scripts-descriptions']['upstream-require'])) {
+        unset($composerJson['scripts-descriptions']['upstream-require']);
+      }
+
+      // This may have been the last item in the scripts-descriptions section, so remove it.
+      if (isset($composerJson['scripts-descriptions']) && empty($composerJson['scripts-descriptions'])) {
+        unset($composerJson['scripts-descriptions']);
+      }
+
+      // enable patching if it isn't already enabled
+      if(! isset($composerJson['extra']['enable-patching'])) {
+        $io->write("<info>Setting enable-patching to true</info>");
+        $composerJson['extra']['enable-patching'] = true;
+      }
+
+      // allow phpstan/extension-installer in preparation for Drupal 10
+      if(! isset($composerJson['config']['allow-plugins']['phpstan/extension-installer'])) {
+        $io->write("<info>Allow phpstan/extension-installer in preparation for Drupal 10</info>");
+        $composerJson['config']['allow-plugins']['phpstan/extension-installer'] = true;
+      }
+
+      // allow php-http/discovery
+      if(! isset($composerJson['config']['allow-plugins']['php-http/discovery'])) {
+        $io->write("<info>Allow php-http/discovery</info>");
+        $composerJson['config']['allow-plugins']['php-http/discovery'] = true;
+      }
+    }
+
+    if(serialize($composerJson) == serialize($originalComposerJson)) {
+      return;
+    }
+
+    // Write the updated composer.json file
+    $composerJsonContents = static::jsonEncodePretty($composerJson);
+    file_put_contents("composer.json", $composerJsonContents . PHP_EOL);
+  }
+
+  /**
+   * jsonEncodePretty
+   *
+   * Convert a nested array into a pretty-printed json-encoded string.
+   *
+   * @param array $data
+   *   The data array to encode
+   * @return string
+   *   The pretty-printed encoded string version of the supplied data.
+   */
+  public static function jsonEncodePretty(array $data) {
+    $prettyContents = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    $prettyContents = preg_replace('#": \[\s*("[^"]*")\s*\]#m', '": [\1]', $prettyContents);
+
+    return $prettyContents;
+  }
+
+  /**
+   * Get current platform.php value.
+   */
+  private static function getCurrentPlatformPhp(Event $event) {
+    $composer = $event->getComposer();
+    $config = $composer->getConfig();
+    $platform = $config->get('platform') ?: [];
+    if (isset($platform['php'])) {
+      return $platform['php'];
+    }
+    return null;
+  }
+
+  /**
+   * Merge the upstream and custom patches into a single file.
+   *
+   * @param PreCommandRunEvent $event
+   *
+   * @return void
+   */
+  public static function writeComposerPatchFile(PreCommandRunEvent $event) {
+    $websparkPath = 'webspark-dependencies/patches.webspark.json';
+    $customPath = 'custom-dependencies/patches.custom.json';
+    $websparkArray = json_decode(file_get_contents($websparkPath), true);
+    $customArray = json_decode(file_get_contents($customPath), true);
+    $combinedArray['patches'] = array_merge_recursive($websparkArray, $customArray);
+    $combinedJson = json_encode($combinedArray);
+
+    file_put_contents("composer.patches.json", $combinedJson . PHP_EOL);
+  }
+}
